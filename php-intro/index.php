67e64f662df42bba09d9c671b554a97e59e6dfe6
gavdaddy69 <60488977+gavdaddy69@users.noreply.github.com>
2020-05-27 13:34:41 +1200

    more things

---
+++ php-intro/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "Hello World";
+    $name = "Ethan Adams";
+    echo $name;
+
+    ?>
+  </body>
+</html>
